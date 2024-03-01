12344ad8fabd63064a612bc9d1be9b8d5c2bf002
krisco6 <161787182+krisco6@users.noreply.github.com>
2024-02-29 22:05:30 -0500

    Update settings.py

--- KRISCO/settings.py
+++ KRISCO/settings.py
@@ -83,10 +83,19 @@ WSGI_APPLICATION = 'KRISCO.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'krisco_sales',
+        'USER': 'root',
+        'PASSWORD': 'manojroot',
+        'HOST': 'mysql-rds-bumpy-orange-czq3.c9okg8a0ahuw.us-west-2.rds.amazonaws.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
